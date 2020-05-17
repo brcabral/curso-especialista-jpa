81dbc0f532f63ebae640877ad6f2e04f6693b45e
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-05-16 23:02:20 -0300

    Abrindo e fechando uma transação

---
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -0,0 +1,18 @@
+package com.algaworks.ecommerce.iniciandocomjpa;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Produto;
+
+public class OperacoesComTransacaoTest extends EntityManagerTest {
+    public void abrirEFecharATransacao() {
+        Produto produto = new Produto();
+
+        entityManager.getTransaction().begin();
+
+//        entityManager.persist(produto);
+//        entityManager.merge(produto);
+//        entityManager.remove(produto);
+
+        entityManager.getTransaction().commit();
+    }
+}
