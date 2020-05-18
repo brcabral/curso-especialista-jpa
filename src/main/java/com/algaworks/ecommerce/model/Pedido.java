660411e80494564866c63061aa802e1f81415e72
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-05-17 23:06:29 -0300

    Exercício: mapeando a classe Pedido

--- src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -4,6 +4,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -20,10 +21,13 @@ public class Pedido {
     @EqualsAndHashCode.Include
     private Integer id;
 
+    @Column(name = "data_pedido")
     private LocalDateTime dataPedido;
 
+    @Column(name = "data_conclusao")
     private LocalDateTime dataConclusao;
 
+    @Column(name = "nota_fiscal_id")
     private Integer notaFiscalId;
 
     private BigDecimal total;
