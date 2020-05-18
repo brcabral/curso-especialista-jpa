0d77850030554ebd2db300de1111d8cb013bf1ae
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-05-17 23:48:46 -0300

    Mapeando objetos embutidos com @Embeddable

---
+++ src/test/java/com/algaworks/ecommerce/mapeamentobasico/MapeamentoObjetoEmbutidoTest.java
@@ -0,0 +1,42 @@
+package com.algaworks.ecommerce.mapeamentobasico;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.EnderecoEntregaPedido;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.StatusPedido;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class MapeamentoObjetoEmbutidoTest extends EntityManagerTest {
+    @Test
+    public void analisarMapeamentoObjetoEmbutido() {
+        EnderecoEntregaPedido endereco = new EnderecoEntregaPedido();
+        endereco.setCep("14800-000");
+        endereco.setLogradouro("Rua São Bento");
+        endereco.setNumero("1234");
+        endereco.setBairro("Centro");
+        endereco.setCidade("Araraquara");
+        endereco.setEstado("MG");
+
+        Pedido pedido = new Pedido();
+        pedido.setId(1);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        pedido.setTotal(new BigDecimal(1000));
+        pedido.setEnderecoEntrega(endereco);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoInserido = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertNotNull(pedidoInserido);
+        Assert.assertNotNull(pedidoInserido.getEnderecoEntrega());
+        Assert.assertNotNull(pedidoInserido.getEnderecoEntrega().getCep());
+    }
+}
