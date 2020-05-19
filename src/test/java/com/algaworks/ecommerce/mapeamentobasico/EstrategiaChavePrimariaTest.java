dea1f0afdaa0017e0c1d3d602d624da0af1d5d1a
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-05-19 18:14:38 -0300

    Conhecendo as estratégias para geração de identificador com @GeneratedValue

---
+++ src/test/java/com/algaworks/ecommerce/mapeamentobasico/EstrategiaChavePrimariaTest.java
@@ -0,0 +1,23 @@
+package com.algaworks.ecommerce.mapeamentobasico;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Categoria;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EstrategiaChavePrimariaTest extends EntityManagerTest {
+    @Test
+    public void testarEstrategiaAuto() {
+        Categoria categoria = new Categoria();
+        categoria.setNome("Eletrônicos");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(categoria);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Categoria categoriaInserida = entityManager.find(Categoria.class, categoria.getId());
+        Assert.assertNotNull(categoriaInserida);
+    }
+}
