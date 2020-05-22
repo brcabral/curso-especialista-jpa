1d72f57b1a0aac3d5102a85c14a561fc4805cfeb
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-05-21 23:03:36 -0300

    Mapeando relacionamentos um-para-muitos com @OneToMany

--- src/main/java/com/algaworks/ecommerce/model/Cliente.java
+++ src/main/java/com/algaworks/ecommerce/model/Cliente.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -21,4 +22,7 @@ public class Cliente {
 
     @Enumerated(EnumType.STRING)
     private SexoCliente sexo;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos;
 }
