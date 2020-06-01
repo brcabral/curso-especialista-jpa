483849e208edce42b78129daf1dbc388b69fad68
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-05-31 23:20:41 -0300

    Mapeando mapas com @ElementCollection

--- src/test/java/com/algaworks/ecommerce/mapeamentoavancao/ElementCollectionTest.java
+++ src/test/java/com/algaworks/ecommerce/mapeamentoavancao/ElementCollectionTest.java
@@ -2,11 +2,13 @@ package com.algaworks.ecommerce.mapeamentoavancao;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Atributo;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Produto;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class ElementCollectionTest extends EntityManagerTest {
     @Test
@@ -28,13 +30,26 @@ public class ElementCollectionTest extends EntityManagerTest {
         entityManager.getTransaction().begin();
 
         Produto produto = entityManager.find(Produto.class, 1);
-        produto.setAtributos(Arrays.asList(new Atributo("tela", "320x600")));
-        produto.setAtributos(Arrays.asList(new Atributo("cor", "preta")));
-
+        produto.setAtributos(Arrays.asList(new Atributo("tela", "320x600"),
+                new Atributo("cor", "preta")));
         entityManager.getTransaction().commit();
         entityManager.clear();
 
         Produto produtoAlterado = entityManager.find(Produto.class, produto.getId());
         Assert.assertFalse(produtoAlterado.getAtributos().isEmpty());
+    }
+
+    @Test
+    public void aplicarContato() {
+        entityManager.getTransaction().begin();
+
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+        cliente.setContatos(Collections.singletonMap("email", "fernando@email.com"));
+
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+
+        Cliente clienteAlterado = entityManager.find(Cliente.class, cliente.getId());
+        Assert.assertEquals("fernando@email.com", clienteAlterado.getContatos().get("email"));
     }
 }
