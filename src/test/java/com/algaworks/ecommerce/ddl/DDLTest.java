ca4dc87475c834a5ca0300218c648e0ad921a533
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-06-09 21:45:16 -0300

    Configurando detalhes da tabela com @Table

---
+++ src/test/java/com/algaworks/ecommerce/ddl/DDLTest.java
@@ -0,0 +1,11 @@
+package com.algaworks.ecommerce.ddl;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import org.junit.Test;
+
+public class DDLTest extends EntityManagerTest {
+    @Test
+    public void gerarDDL() {
+        // Teste vazio... Somente para visualizar o script do DDL
+    }
+}
