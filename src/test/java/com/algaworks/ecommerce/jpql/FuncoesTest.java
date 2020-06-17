7695dbfac212316d77bcd791c018b3570046c014
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-06-16 22:22:35 -0300

    Usando funções para coleções

--- src/test/java/com/algaworks/ecommerce/jpql/FuncoesTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/FuncoesTest.java
@@ -47,4 +47,15 @@ public class FuncoesTest extends EntityManagerTest {
 
         lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
     }
+
+    @Test
+    public void aplicarFuncaoColecao() {
+        String jpql = "select size(p.itensPedido) from Pedido p where size(p.itensPedido) > 1";
+
+        TypedQuery<Integer> typedQuery = entityManager.createQuery(jpql, Integer.class);
+        List<Integer> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(size -> System.out.println(size));
+    }
 }
