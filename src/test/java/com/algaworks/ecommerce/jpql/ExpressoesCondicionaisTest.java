6145f37cc97d7c989410050b1f6ce990c1c49277
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-06-17 11:38:28 -0300

    Usando a expressão case

--- src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -108,4 +108,39 @@ public class ExpressoesCondicionaisTest extends EntityManagerTest {
         // Assert.assertTrue(lista.size() == 2);
         Assert.assertFalse(lista.isEmpty());
     }
+
+    @Test
+    public void usarExpressaoCase() {
+        String jpql = "select p.id, " +
+                "case p.status " +
+                "   when 'PAGO' then 'Está pago' " +
+                "   when 'CANCELADO' then 'Foi cancelado' " +
+                "   else 'Está aguardando' " +
+                "end " +
+                "from Pedido p";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+
+    @Test
+    public void usarExpressaoCaseComType() {
+        // Utiliza o tipo da classe
+        String jpql = "select p.id, " +
+                "case type(p.pagamento) " +
+                "   when PagamentoBoleto then 'Pago com boleto' " +
+                "   when PagamentoCartao then 'Pago com cartão' " +
+                "   else 'Ainda não foi pago' " +
+                "end " +
+                "from Pedido p";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
 }
