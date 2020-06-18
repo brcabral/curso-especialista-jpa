edd8be91c22dd15060acd9591aa8f3f47a0f3d0c
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-06-17 23:03:23 -0300

    Criando subqueries com a expressão in

--- src/test/java/com/algaworks/ecommerce/jpql/SubqueriesTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/SubqueriesTest.java
@@ -62,4 +62,19 @@ public class SubqueriesTest extends EntityManagerTest {
 
         lista.forEach(obj -> System.out.println("ID: " + obj.getId() + ", Nome: " + obj.getNome()));
     }
+
+    @Test
+    public void pesquisarComIn() {
+        String jpql = "select p from Pedido p " +
+                "where p.id in (select p2.id from ItemPedido i2 " +
+                "                   join i2.pedido p2 join i2.produto pro2 " +
+                "               where pro2.preco > 100)";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(p -> System.out.println("ID: " + p.getId() + ", total: " + p.getTotal()));
+    }
 }
