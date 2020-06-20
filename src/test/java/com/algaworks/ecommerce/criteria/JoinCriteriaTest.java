a979879152a52121061a4930d77488fd3c68df48
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-06-19 22:24:48 -0300

    Usando a cláusula on no join

--- src/test/java/com/algaworks/ecommerce/criteria/JoinCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/JoinCriteriaTest.java
@@ -3,6 +3,7 @@ package com.algaworks.ecommerce.criteria;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Pagamento;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.StatusPagamento;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -26,7 +27,25 @@ public class JoinCriteriaTest extends EntityManagerTest {
         TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
         List<Pedido> lista = typedQuery.getResultList();
 
-        Assert.assertTrue(lista.size() == 4);
-        // Assert.assertFalse(lista.isEmpty());
+        // Assert.assertTrue(lista.size() == 4);
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void fazerJoinComOn() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento");
+        joinPagamento.on(criteriaBuilder
+                .equal(joinPagamento.get("status"), StatusPagamento.PROCESSANDO));
+
+        criteriaQuery.select(root);
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+
+        // Assert.assertTrue(lista.size() == 2);
+        Assert.assertFalse(lista.isEmpty());
     }
 }
