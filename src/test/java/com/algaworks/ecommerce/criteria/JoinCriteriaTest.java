cd3407b77fae794e7392c965761152e084330463
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-06-19 22:57:56 -0300

    Exercício: Consultando pedidos com um produto específico

--- src/test/java/com/algaworks/ecommerce/criteria/JoinCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/JoinCriteriaTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.criteria;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.ItemPedido;
 import com.algaworks.ecommerce.model.Pagamento;
 import com.algaworks.ecommerce.model.Pedido;
 import com.algaworks.ecommerce.model.StatusPagamento;
@@ -78,5 +79,25 @@ public class JoinCriteriaTest extends EntityManagerTest {
         TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
         Pedido pedido = typedQuery.getSingleResult();
         Assert.assertNotNull(pedido);
+    }
+
+    @Test
+    public void buscarPedidoComProdutoEspecifico() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, ItemPedido> joinItensPedido = root.join("itensPedido");
+        // Join<Pedido, ItemPedido> joinItensPedidoProduto = root.join("itensPedido")
+        //      .join("produto");
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.equal(joinItensPedido.get("produto").get("id"), 1));
+        // criteriaQuery.where(criteriaBuilder.equal(joinItensPedidoProduto.get("id"), 1));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+
+        // Assert.assertTrue(lista.size() == 4);
+        Assert.assertFalse(lista.isEmpty());
     }
 }
