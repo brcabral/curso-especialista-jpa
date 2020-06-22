7a31e317cbaebb0bb363b9c7d55e4c97e255059f
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-06-22 20:51:56 -0300

    Usando funções para datas

--- src/main/java/com/algaworks/ecommerce/model/PagamentoBoleto.java
+++ src/main/java/com/algaworks/ecommerce/model/PagamentoBoleto.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -14,4 +15,7 @@ import javax.persistence.Entity;
 public class PagamentoBoleto extends Pagamento {
     @Column(name = "codigo_barras", length = 100)
     private String codigoBarras;
+
+    @Column(name = "data_vencimento")
+    private LocalDate dataVencimento;
 }
