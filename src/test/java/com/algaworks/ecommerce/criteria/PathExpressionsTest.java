e8e86a8125cb93461423c676ae2b46200c2bc3f5
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-06-25 23:11:42 -0300

    Exercício: consultando pedidos com produto específico

--- src/test/java/com/algaworks/ecommerce/criteria/PathExpressionsTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/PathExpressionsTest.java
@@ -1,15 +1,14 @@
 package com.algaworks.ecommerce.criteria;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.model.Cliente_;
-import com.algaworks.ecommerce.model.Pedido;
-import com.algaworks.ecommerce.model.Pedido_;
+import com.algaworks.ecommerce.model.*;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
@@ -21,7 +20,24 @@ public class PathExpressionsTest extends EntityManagerTest {
         Root<Pedido> root = criteriaQuery.from(Pedido.class);
 
         criteriaQuery.select(root);
-        criteriaQuery.where(criteriaBuilder.like(root.get(Pedido_.cliente).get(Cliente_.nome), "M%"));
+        criteriaQuery.where(criteriaBuilder.like(
+                root.get(Pedido_.cliente).get(Cliente_.nome), "M%"));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void buscarPedidoComProdutoEspecifico() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, ItemPedido> joinItemPedido = root.join("itensPedido");
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.equal(
+                joinItemPedido.get(ItemPedido_.produto).get(Produto_.id), 1));
 
         TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
         List<Pedido> lista = typedQuery.getResultList();
