1e8ade005c725c32c76ed6068855ef8ea5cf525a
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-06-26 23:25:01 -0300

    Criando subqueries

---
+++ src/test/java/com/algaworks/ecommerce/criteria/SubqueriesCriteriaTest.java
@@ -0,0 +1,70 @@
+package com.algaworks.ecommerce.criteria;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Pedido_;
+import com.algaworks.ecommerce.model.Produto;
+import com.algaworks.ecommerce.model.Produto_;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Subquery;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class SubqueriesCriteriaTest extends EntityManagerTest {
+    @Test
+    public void produtosMaisCaros() {
+        // Produto(s) mais caro(s) da base
+        // String jpql = "select p from Produto p " +
+        //        "where p.preco = (select max(preco) from Produto)";
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(root);
+
+        Subquery<BigDecimal> subquery = criteriaQuery.subquery(BigDecimal.class);
+        Root<Produto> subqueryRoot = subquery.from(Produto.class);
+        subquery.select(criteriaBuilder.max(subqueryRoot.get(Produto_.preco)));
+
+        criteriaQuery.where(criteriaBuilder.equal(root.get(Produto_.preco), subquery));
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Produto> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(prod -> System.out.println(
+                "ID: " + prod.getId() + ", Nome: " + prod.getNome() + ", Preço: " + prod.getPreco()));
+    }
+
+    @Test
+    public void pedidoAcimaDaMedia() {
+        // Todos os pedidos acima da média de venda
+        // String jpql = "select p from Pedido p " +
+        //      "where p.total > (select avg(total) from Pedido)";
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root);
+
+        Subquery<BigDecimal> subquery = criteriaQuery.subquery(BigDecimal.class);
+        Root<Pedido> subqueryRoot = subquery.from(Pedido.class);
+        subquery.select(criteriaBuilder.avg(subqueryRoot.get(Pedido_.total)).as(BigDecimal.class));
+
+        criteriaQuery.where(criteriaBuilder.greaterThan(root.get(Pedido_.total), subquery));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(p -> System.out.println("ID: " + p.getId() + ", Total: " + p.getTotal()));
+    }
+}
