0a089284cbb368b262ec988b31f5bb99507b7c25
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-07-09 21:45:00 -0300

    Utilizando metamodel com Entity Graph

--- src/test/java/com/algaworks/ecommerce/detalhesimportantes/EntityGraphTest.java
+++ src/test/java/com/algaworks/ecommerce/detalhesimportantes/EntityGraphTest.java
@@ -2,7 +2,9 @@ package com.algaworks.ecommerce.detalhesimportantes;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Cliente;
+import com.algaworks.ecommerce.model.Cliente_;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Pedido_;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -47,6 +49,22 @@ public class EntityGraphTest extends EntityManagerTest {
         Subgraph<Cliente> subgraphCliente = entityGraph
                 .addSubgraph("cliente", Cliente.class);
         subgraphCliente.addAttributeNodes("nome", "cpf");
+
+        TypedQuery<Pedido> typedQuery = entityManager
+                .createQuery("select p from Pedido p", Pedido.class);
+        typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void buscarAtributosEssenciaisDePedidoComJpqlESubgraphComMetamodel() {
+        EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
+        entityGraph.addAttributeNodes(Pedido_.dataCriacao, Pedido_.status, Pedido_.total);
+
+        Subgraph<Cliente> subgraphCliente = entityGraph
+                .addSubgraph(Pedido_.cliente);
+        subgraphCliente.addAttributeNodes(Cliente_.nome, Cliente_.cpf);
 
         TypedQuery<Pedido> typedQuery = entityManager
                 .createQuery("select p from Pedido p", Pedido.class);
