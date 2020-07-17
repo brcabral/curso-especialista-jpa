c02b790a9cb803227c3af6da9b69a3e44dcf54f2
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-07-16 22:31:07 -0300

    Implementando multitenancy com abordagem por schema

---
+++ src/test/java/com/algaworks/ecommerce/multitenant/MultiTenantTest.java
@@ -0,0 +1,26 @@
+package com.algaworks.ecommerce.multitenant;
+
+import com.algaworks.ecommerce.EntityManagerFactoryTest;
+import com.algaworks.ecommerce.hibernate.EcmCurrentTenantIdentifierResolver;
+import com.algaworks.ecommerce.model.Produto;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+
+public class MultiTenantTest extends EntityManagerFactoryTest {
+    @Test
+    public void usarAbordagemPorSchema() {
+        EcmCurrentTenantIdentifierResolver.setTenantIdentifier("ecommerce_jpa");
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        Produto produto1 = entityManager1.find(Produto.class, 1);
+        Assert.assertEquals("Kindle", produto1.getNome());
+        entityManager1.close();
+
+        EcmCurrentTenantIdentifierResolver.setTenantIdentifier("ecommerce_multitenancy");
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+        Produto produto2 = entityManager2.find(Produto.class, 1);
+        Assert.assertEquals("Kindle Paperwhite", produto2.getNome());
+        entityManager2.close();
+    }
+}
