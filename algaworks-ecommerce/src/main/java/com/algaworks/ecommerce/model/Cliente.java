deeba0a61910d07638d255f96e8ad1d731c05c18
Breno Cabral <brcabral@gmail.com>
2024-05-02 00:19:07 -0300

    3.5. Mapeando enumerações com @Enumerated

--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Cliente.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Cliente.java
@@ -1,8 +1,6 @@
 package com.algaworks.ecommerce.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,5 +17,6 @@ public class Cliente {
 
     private String nome;
 
+    @Enumerated(EnumType.STRING)
     private SexoCliente sexo;
 }
--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/PagamentoBoleto.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/PagamentoBoleto.java
@@ -1,9 +1,6 @@
 package com.algaworks.ecommerce.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,6 +18,7 @@ public class PagamentoBoleto {
     @Column(name = "pedido_id")
     private Integer pedidoId;
 
+    @Enumerated(EnumType.STRING)
     private StatusPagamento status;
 
     @Column(name = "codigo_barras")
