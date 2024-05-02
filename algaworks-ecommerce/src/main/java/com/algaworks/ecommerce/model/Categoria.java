d0a3f5c00ca79d1836f58a92cd1a2718ec810819
Breno Cabral <brcabral@gmail.com>
2024-05-02 03:12:38 -0300

    4.6. Mapeando autorelacionamentos com @ManyToOne e @OneToMany

--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -5,6 +5,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -18,6 +20,10 @@ public class Categoria {
 
     private String nome;
 
-    @Column(name = "categoria_pai_id")
-    private Integer categoriaPaiId;
+    @ManyToOne
+    @JoinColumn(name = "categoria_pai_id")
+    private Categoria categoriaPai;
+
+    @OneToMany(mappedBy = "categoriaPai")
+    private List<Categoria> categorias;
 }
