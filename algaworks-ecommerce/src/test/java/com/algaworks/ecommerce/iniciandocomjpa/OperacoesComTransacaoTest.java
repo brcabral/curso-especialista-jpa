d7a1894360f4e0e4d434298bfc84f39147949397
Breno Cabral <brcabral@gmail.com>
2024-05-02 01:48:42 -0300

    3.11. Exercício: corrigindo classes de testes

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -12,7 +12,6 @@ public class OperacoesComTransacaoTest extends EntityManagerTest {
     public void inserirOPrimeiroObjeto() {
         Produto produto = new Produto();
 
-        produto.setId(2);
         produto.setNome("Câmera Canon");
         produto.setDescricao("A melhor definição para suas fotos.");
         produto.setPreco(new BigDecimal(5000));
@@ -87,18 +86,17 @@ public class OperacoesComTransacaoTest extends EntityManagerTest {
     public void inserirObjetoComMerge() {
         Produto produto = new Produto();
 
-        produto.setId(4);
         produto.setNome("Microfone Rode Videmic");
         produto.setDescricao("A melhor qualidade de som.");
         produto.setPreco(new BigDecimal(1000));
 
         entityManager.getTransaction().begin();
-        entityManager.merge(produto);
+        Produto produtoSalvo = entityManager.merge(produto);
         entityManager.getTransaction().commit();
 
         entityManager.clear();
 
-        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Produto produtoVerificacao = entityManager.find(Produto.class, produtoSalvo.getId());
         Assertions.assertNotNull(produtoVerificacao);
     }
 
@@ -106,7 +104,6 @@ public class OperacoesComTransacaoTest extends EntityManagerTest {
     public void mostrarDifencaPersistMerge() {
         // Persistir via persist
         Produto produtoPersist = new Produto();
-        produtoPersist.setId(5);
         produtoPersist.setNome("Smartphone One Plus");
         produtoPersist.setDescricao("O processador mais rápido.");
         produtoPersist.setPreco(new BigDecimal(2000));
@@ -127,7 +124,6 @@ public class OperacoesComTransacaoTest extends EntityManagerTest {
 
         // Persistir via merge
         Produto produtoMerge = new Produto();
-        produtoMerge.setId(6);
         produtoMerge.setNome("Notebook Dell");
         produtoMerge.setDescricao("O melhor da categoria.");
         produtoMerge.setPreco(new BigDecimal(2000));
--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/iniciandocomjpa/PrimeiroCrudTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/iniciandocomjpa/PrimeiroCrudTest.java
@@ -10,7 +10,6 @@ public class PrimeiroCrudTest extends EntityManagerTest {
     @Test
     public void inserirRegistro() {
         Cliente cliente = new Cliente();
-        cliente.setId(3);
         cliente.setNome("José Lucas");
 
         entityManager.getTransaction().begin();
