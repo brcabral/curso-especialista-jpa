94ad59dc6d1ab98137b33e3f917c73067dc78a8e
Breno Cabral <brcabral@gmail.com>
2024-05-05 15:18:15 -0300

    5.1. Estados e ciclo de vida dos objetos

---
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/conhecendoentitymanager/EstadosECicloDeVidaTest.java
@@ -0,0 +1,27 @@
+package com.algaworks.ecommerce.conhecendoentitymanager;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Categoria;
+import org.junit.jupiter.api.Test;
+
+public class EstadosECicloDeVidaTest extends EntityManagerTest {
+    @Test
+    public void analisarEstados() {
+        // Estado novo (transient)
+        Categoria categoriaNovo = new Categoria();
+
+        // O retorno passa para o Estado gerenciado (managed)
+        Categoria categoriaGerenciadaMerge = entityManager.merge(categoriaNovo);
+
+        Categoria categoriaGerenciada = entityManager.find(Categoria.class, 1);
+
+        // Estado de remoção (removed)
+        entityManager.remove(categoriaGerenciada);
+
+        // Ao chamar o persist, o objeto vai para o estado gerenciado
+        entityManager.persist(categoriaGerenciada);
+
+        // Estado desanexar (detached)
+        entityManager.detach(categoriaGerenciada);
+    }
+}
