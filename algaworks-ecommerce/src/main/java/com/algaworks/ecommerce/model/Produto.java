33fdad1896598d6362405a7802e9a0b607cf87dc
Breno Cabral <brcabral@gmail.com>
2024-05-19 18:05:03 -0300

    6.10. Mapeando coleções de objetos embutidos com @ElementCollection

--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -48,4 +48,9 @@ public class Produto {
             joinColumns = @JoinColumn(name = "produto_id"))
     @Column(name = "tag")
     private List<String> tags;
+
+    @ElementCollection
+    @CollectionTable(name = "produto_atributo",
+            joinColumns = @JoinColumn(name = "produto_id"))
+    private List<Atributo> atributos;
 }
--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/mapeamentoavancao/ElementCollectionTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/mapeamentoavancao/ElementCollectionTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.mapeamentoavancao;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Atributo;
 import com.algaworks.ecommerce.model.Produto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -21,5 +22,20 @@ public class ElementCollectionTest extends EntityManagerTest {
 
         Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
         Assertions.assertFalse(produtoVerificacao.getTags().isEmpty());
+    }
+
+    @Test
+    public void aplicarAtributos() {
+        entityManager.getTransaction().begin();
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setAtributos(Arrays.asList(new Atributo("tela", "320x600")));
+
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assertions.assertFalse(produtoVerificacao.getAtributos().isEmpty());
     }
 }
