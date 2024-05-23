3a13682f7f88ffaf36e677d0a2b0cbc273cb4479
Breno Cabral <brcabral@gmail.com>
2024-05-22 23:04:49 -0300

    6.16. Entendendo a diferença entre estender uma entidade abstrata e usar a anotação @MappedSuperclass

--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
@@ -9,14 +9,7 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name = "pagamento_cartao")
-public class PagamentoCartao extends EntidadeBaseInteger {
-    @MapsId
-    @OneToOne(optional = false)
-    @JoinColumn(name = "pedido_id")
-    private Pedido pedido;
-
-    @Enumerated(EnumType.STRING)
-    private StatusPagamento status;
-
-    private String numero;
+public class PagamentoCartao extends Pagamento {
+    @Column(name = "numero_cartao")
+    private String numeroCartao;
 }
