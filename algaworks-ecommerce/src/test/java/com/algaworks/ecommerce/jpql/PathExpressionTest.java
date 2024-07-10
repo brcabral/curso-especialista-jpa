3dd4161046d0d46a5d67448d39781e695e754ef5
Breno Cabral <brcabral@gmail.com>
2024-07-09 22:03:08 -0300

    9.10. Exercício: consultando pedidos com produto específico

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/PathExpressionTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/PathExpressionTest.java
@@ -1,6 +1,8 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
 import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,5 +16,15 @@ public class PathExpressionTest extends EntityManagerTest {
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
         List<Object[]> lista = typedQuery.getResultList();
         Assertions.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void buscarPedidosComProdutoEspecifico() {
+        String jpql = "select p from Pedido p join p.itens i where i.id.produtoId = 1";
+        // String jpql = "select p from Pedido p join p.itens i where i.produto.id = 1";
+        // String jpql = "select p from Pedido p join p.itens i join i.produto pro where pro.id = 1";
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        List<Pedido> listaProduto = typedQuery.getResultList();
+        Assertions.assertFalse(listaProduto.isEmpty());
     }
 }
