d0589063d76fd3f80c0c584b9280b9ed91ba9001
Breno Cabral <brcabral@gmail.com>
2024-09-22 16:43:08 -0300

    9.28. Agrupando o registros com group by

---
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
@@ -0,0 +1,47 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class GroupByTest extends EntityManagerTest {
+    @Test
+    public void quantidadeProdutoPorCategoria() {
+        String jpql = "select c.nome, count(p.id) from Categoria c join c.produtos p group by c.id";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+
+    @Test
+    public void totalVendasPorMes() {
+        String jpql = "select year(p.dataCriacao), function('monthname', p.dataCriacao), sum(p.total) " +
+                "from Pedido p " +
+                "group by year(p.dataCriacao), month(p.dataCriacao)";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1] + ", " + arr[2]));
+    }
+
+    @Test
+    public void totalVendasPorCategoria() {
+        String jpql = "select c.nome, sum(ip.precoProduto * ip.quantidade) " +
+                "from ItemPedido ip join ip.produto pro join pro.categorias c " +
+                "group by c.id";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+}
