c86e7068166307d4ec04a9f2aab02e44461688a8
Breno Cabral <brcabral@gmail.com>
2024-09-22 17:03:15 -0300

    9.29. Exercício: usando group by

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
@@ -44,4 +44,34 @@ public class GroupByTest extends EntityManagerTest {
 
         lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
     }
+
+    @Test
+    public void totalVendasPorCliente() {
+        String jpql = "select c.nome, sum(ip.precoProduto * ip.quantidade)" +
+                "from Pedido p join p.cliente c join p.itens ip " +
+                "group by c.id";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+
+    @Test
+    public void totalVendasPorDiaPorCategoria() {
+        String jpql = "select concat(day(p.dataCriacao), '/', month(p.dataCriacao), '/', year(p.dataCriacao)), " +
+                "c.nome, sum(ip.precoProduto * ip.quantidade) " +
+                "from ItemPedido ip " +
+                "join ip.produto pro " +
+                "join pro.categorias c " +
+                "join ip.pedido p " +
+                "group by p.dataCriacao, c.id";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1] + ": " + arr[2]));
+    }
 }
