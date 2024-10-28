9c8942c809ac39b19acff20e8661ff8bc87e4650
Breno Cabral <brcabral@gmail.com>
2024-10-27 22:29:38 -0300

    9.35. Criando subqueries

---
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/SubqueriesTest.java
@@ -0,0 +1,61 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class SubqueriesTest extends EntityManagerTest {
+    @Test
+    public void produtosMaisCaros() {
+        String jpql = "select p from Produto p " +
+                "where p.preco = (select max(preco) from Produto)";
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(jpql, Produto.class);
+        List<Produto> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(p -> System.out.println("Nome: " + p.getNome() + ", preço: " + p.getPreco()));
+    }
+
+    @Test
+    public void pedidoAcimaDaMedia() {
+        String jpql = "select p from Pedido p " +
+                "where p.total > (select avg(total) from Pedido)";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(p -> System.out.println("ID: " + p.getId() + ", total: " + p.getTotal()));
+    }
+
+    @Test
+    public void bonsClientes1() {
+        String jpql = "select c from Cliente c " +
+                "where 500 < (select sum(p.total) from Pedido p where p.cliente = c)";
+
+        TypedQuery<Cliente> typedQuery = entityManager.createQuery(jpql, Cliente.class);
+        List<Cliente> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println("ID: " + obj.getId() + ", Nome: " + obj.getNome()));
+    }
+
+    @Test
+    public void bonsClientes2() {
+        String jpql = "select c from Cliente c " +
+                "where 500 < (select sum(p.total) from c.pedidos p)";
+
+        TypedQuery<Cliente> typedQuery = entityManager.createQuery(jpql, Cliente.class);
+        List<Cliente> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println("ID: " + obj.getId() + ", Nome: " + obj.getNome()));
+    }
+}
