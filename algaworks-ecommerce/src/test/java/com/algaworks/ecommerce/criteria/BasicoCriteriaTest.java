ea1895ac2bf6937ad6941efa37702fa787826a87
Breno Cabral <brcabral@gmail.com>
2025-01-21 16:32:09 -0300

    10.2. Selecionando um atributo da entidade como retorno da consulta

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.criteria;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Pedido;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
@@ -8,6 +9,8 @@ import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 
 public class BasicoCriteriaTest extends EntityManagerTest {
     @Test
@@ -22,5 +25,33 @@ public class BasicoCriteriaTest extends EntityManagerTest {
         TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
         Pedido pedido = typedQuery.getSingleResult();
         Assertions.assertNotNull(pedido);
+    }
+
+    @Test
+    public void selecionarUmAtributoClienteParaRetorno() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Cliente> criteriaQuery = criteriaBuilder.createQuery(Cliente.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root.get("cliente"));
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+        TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
+        Cliente cliente = typedQuery.getSingleResult();
+        Assertions.assertEquals("Fernando Medeiros", cliente.getNome());
+    }
+
+    @Test
+    public void selecionarUmAtributoBigDecimalParaRetorno() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<BigDecimal> criteriaQuery = criteriaBuilder.createQuery(BigDecimal.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root.get("total"));
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+        TypedQuery<BigDecimal> typedQuery = entityManager.createQuery(criteriaQuery);
+        BigDecimal total = typedQuery.getSingleResult();
+        Assertions.assertEquals(new BigDecimal("2398.00"), total);
     }
 }
