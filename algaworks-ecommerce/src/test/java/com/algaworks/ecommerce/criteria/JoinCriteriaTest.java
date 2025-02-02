f9b0201363af0df6a0ba8606b9f61105e2941d32
Breno Cabral <brcabral@gmail.com>
2025-02-02 15:23:52 -0300

    10.7. Fazendo inner join entre as entidades

---
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/criteria/JoinCriteriaTest.java
@@ -0,0 +1,32 @@
+package com.algaworks.ecommerce.criteria;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pagamento;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Root;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class JoinCriteriaTest extends EntityManagerTest {
+    @Test
+    public void fazerJoin() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento");
+
+        criteriaQuery.select(root);
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+        // Assertions.assertFalse(lista.isEmpty());
+        Assertions.assertTrue(lista.size() == 4);
+    }
+}
